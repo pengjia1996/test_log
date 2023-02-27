14dbd4c4faf08ffc9eb253bc29f33117193499d5
pengjia <2776248656>
2023-02-27 18:51:17 +0800

    添加源码测试

---
+++ log4j_source_code/src/test/java/com/pengjia/test/Test.java
@@ -0,0 +1,18 @@
+package com.pengjia.test;
+
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author pengjia
+ * @date 2023/2/27 17:29
+ */
+public class Test {
+    private static final Logger log=Logger.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        log.info("aaaaaaaa");
+    }
+}
