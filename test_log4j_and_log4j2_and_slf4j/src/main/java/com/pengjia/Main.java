c6920b725e8310dfa22279ef5c4b30be906acad4
pengjia <2776248656>
2023-07-24 11:16:36 +0800

    测试log4j和log4j2,slf4j

--- test_log4j_and_log4j2_and_slf4j/src/main/java/com/pengjia/Main.java
+++ test_log4j_and_log4j2_and_slf4j/src/main/java/com/pengjia/Main.java
@@ -1,7 +1,18 @@
 package com.pengjia;
 
+import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger log=Logger.getLogger(Main.class);
+
+    private static final org.apache.logging.log4j.Logger log2 = LogManager.getLogger(Main.class);//log4j2的写法 import org.apache.logging.log4j.xxx
+    private static final org.slf4j.Logger log1= LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("这是log4j对象打印");
+        log1.info("这是slf4j对象打印");
+        log2.info("这是log4j2对象打印");
     }
 }
